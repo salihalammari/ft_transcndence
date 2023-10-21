a93ad32bf694db363661f20d8addcad60d7aed0a
saliha lammari <75095222+salihalammari@users.noreply.github.com>
2023-10-21 23:15:37 +0100

    Update auth.module.ts

--- backend/src/auth/auth.module.ts
+++ backend/src/auth/auth.module.ts
@@ -1,17 +1,13 @@
-import { Injectable } from '@nestjs/common'
-import { User42Dto } from '../user/dto/user42.dto'
-import { UserService } from '../user/user.service'
-import { User } from '../user/entities/user.entity'
+import { HttpModule } from '@nestjs/axios'
+import { forwardRef, Module } from '@nestjs/common'
+import { UserModule } from '../user/user.module'
+import { AuthController } from './auth.controller'
+import { AuthService } from './auth.service'
+import { IntraStrategy } from './strategy/auth.strategy'
 
-@Injectable()
-export class AuthService {
-	constructor(private userService: UserService) {}
-
-	async validateUser(userData: User42Dto): Promise<User> {
-		return this.userService.validateUser42(userData)
-	}
-
-	createUser(userData: User42Dto): Promise<User> {
-		return this.userService.createUser42(userData)
-	}
-}
+@Module({
+	imports: [forwardRef(() => UserModule), HttpModule],
+	controllers: [AuthController],
+	providers: [IntraStrategy, AuthService],
+})
+export class AuthModule {}
